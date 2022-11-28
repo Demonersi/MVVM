aef0a07f7ddd26f50946f6edf27aa6b12937b69c
student <student@Y5-11>
2022-11-28 19:49:06 +0300

    Добавьте файлы проекта.

---
+++ MVVM/ApplicationViewModel.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using MVVM;
+
+namespace MVVM
+{
+    internal class ApplicationViewModel : INotifyPropertyChanged
+    {
+
+        Phone selectedPhone;
+        IFileService fileService;
+        IDialogService dialogService;
+        public ObservableCollection<Phone> Phones { get; set; }
+        //command Save file
+        private RelayCommand saveCommand;
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                return saveCommand ??
+                    (saveCommand = new RelayCommand(obj =>
+                    {
+                        try
+                        {
+                            if (dialogService.SaveFileDialog() == true)
+                            {
+                                fileService.Save(dialogService.FilePath, Phones.ToList());
+                                dialogService.ShowMessage("Файл сохранен");
+
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            dialogService.ShowMessage(ex.Message);
+                        }
+                    }));
+            }
+        }
+        //command Open file
+        private RelayCommand openCommand;
+        public RelayCommand OpenCommand
+        {
+            get
+            {
+                return openCommand ??
+                    (openCommand = new RelayCommand(obj =>
+                    {
+                        try
+                        {
+                            if (dialogService.OpenFileDialog() == true)
+                            {
+                                var phones = fileService.Open(dialogService.FilePath);
+                                Phones.Clear();
+                                foreach (var p in phones)
+                                    Phones.Add(p);
+                                dialogService.ShowMessage("File is opened");
+
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            dialogService.ShowMessage(ex.Message);
+                        }
+                    }));
+            }
+        }
+        //command Add
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                    (addCommand = new RelayCommand(obj =>
+                    {
+                        Phone phone = new Phone();
+                        Phones.Insert(0, phone);
+                        SelectedPhone = phone;
+                    }));
+            }
+        }
+        //command Relay
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                    (removeCommand = new RelayCommand(obj =>
+                    {
+                        Phone? phone = obj as Phone;
+                        if (phone != null)
+                        {
+                            Phones.Remove(phone);
+                        }
+                    },
+                    (obj) => Phones.Count > 0));
+            }
+        }
+        private RelayCommand doubleCommand;
+        public RelayCommand DoubleCommand
+        {
+            get
+            {
+                return doubleCommand ??
+                    (doubleCommand = new RelayCommand(obj =>
+                    {
+                        Phone phone = obj as Phone;
+                        if (phone != null)
+                        {
+                            Phone phoneCopy = new Phone(phone.Title, phone.Company, phone.Price);
+                            Phones.Insert(0, phoneCopy);
+                        }
+                    }));
+            }
+        }
+
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel(IDialogService dialogService, IFileService fileService)
+        {
+            this.dialogService = dialogService;
+            this.fileService = fileService;
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone("IPhone 14", "Apple", 125000 ),
+                new Phone("Galaxy S22 Ultra", "Samsung", 90000),
+                new Phone("12 Lite", "Xiaomi", 35000)
+            };
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+}
---
+++ MVVM/JsonFileService.cs
@@ -0,0 +1,37 @@
+﻿using MVVM;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    internal class JsonFileService : IFileService
+    {
+        public List<Phone> Open(string filename)
+        {
+            filename += ".json";
+            List<Phone> phones = new List<Phone>();
+            DataContractJsonSerializer jsonFarmatter = new DataContractJsonSerializer(typeof(List<Phone>));
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                phones = jsonFarmatter.ReadObject(fs) as List<Phone>;
+            }
+            return phones;
+        }
+
+        public void Save(string filename, List<Phone> phonesList)
+        {
+            filename += ".json";
+            DataContractJsonSerializer jsonFarmatter = new DataContractJsonSerializer(typeof(List<Phone>));
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                jsonFarmatter.WriteObject(fs, phonesList);
+            }
+        }
+    }
+}
